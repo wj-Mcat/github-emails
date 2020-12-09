2b485ba1cda332aa2a110103bf4e9fb98f89beb8
wjmcat <1435130236@qq.com>
2020-12-09 15:09:53 +0800

    🔥 update version to 0.0.2

--- examples/get_emails.py
+++ examples/get_emails.py
@@ -1,14 +1,13 @@
 """examples to get emails from repo"""
-from pprint import pprint
-
-from github_emails import GithubApi, GithubOptions
+from github_emails import GithubApi
 
 
 def main():
     """test the `get emails` ability"""
-    github = GithubApi(token='7685a7a6eb0b3e9c91cf66869a4721b53df2d468')
-    emails = github.get_repo_stargazers('wechaty', 'python-wechaty')
-    pprint(emails)
+    github = GithubApi(token='')
+    stargazers = github.stargazers('wechaty', 'python-wechaty')
+    for stargazer in stargazers:
+        github.emails(stargazer)
 
 
 if __name__ == '__main__':
